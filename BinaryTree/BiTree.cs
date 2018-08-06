19313bcc591ac68f7ea0a49661fcdcd21a1c2e83
FilipHojgaard <filip.hojgaard@gmail.com>
2018-08-06 16:45:16 +0200

    Initial commit, programmet virker, uden at kunne tegne

---
+++ BinaryTree/BiTree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree {
+    class BiTree {
+        int? value;
+        BiTree left;
+        BiTree right;
+
+        // Insert - indsætter en værdi i en binær træ struktur. 
+        public void Insert(int val) {
+            if (this.value == null){
+                Console.WriteLine("Første knude");
+                this.value = val;
+                return;
+            }
+            else if (val < this.value) {
+                if(left == null) {
+                    left = new BiTree();
+                    left.value = val;
+                } else {
+                    left.Insert(val);
+                }
+            }
+
+            else if (val > this.value) {
+                if(right == null) {
+                    right = new BiTree();
+                    right.value = val;
+                } else {
+                    right.Insert(val);
+                }
+            } else {
+                Console.WriteLine("ERROR: Insert value doesnt match anything");
+            }
+        }
+
+        // min() - Printer det mindste tal i datastrukturen
+        public int min() {
+            if (left == null) {
+                return (int)this.value;
+            } else {
+                return left.min();
+            }
+        }
+
+        // max() - Printer det højeste tal i datastrukturen
+        public int max() {
+            if (right == null) {
+                return (int)this.value;
+            } else {
+            return right.max();
+
+            }
+        }
+
+        // find() - Checks if a number is inserted in the datastructure
+        public bool find(int val) {
+            if (this.value == val) {
+                return true;
+            }else if (val < this.value && left != null){
+                return left.find(val);
+            }else if(val > this.value && right != null) {
+                return right.find(val);
+            } else {
+                return false;
+            }
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree {
+    class Program {
+        static bool loop = true;
+        static BiTree tree = new BiTree();
+
+
+        static void Main(string[] args) {;
+
+            while (loop) {
+                Console.Write(DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second);
+                Console.Write(" $");
+                Parser(Lexer(Console.ReadLine()));
+
+            }
+        }
+
+        // Lexer - Sammensætter brugerens kommando fra prompten til tokens
+        static public string[] Lexer(string text) {
+            string[] tokens;
+            string tempOrd = "";
+            int ordCount = 0;
+
+            int words = 1;
+            for (int i = 0; i < text.Length; i++) {
+                if (text[i].Equals(' ')) {
+                    words++;
+                }
+            }
+            tokens = new string[words];
+
+            for (int i = 0; i < text.Length; i++) {
+                if (!text[i].Equals(' ')){
+                    tempOrd += text[i];
+                } else {
+                    tokens[ordCount] = tempOrd;
+                    ordCount += 1;
+                    tempOrd = "";
+                }
+            }
+            tokens[ordCount] = tempOrd;
+            return tokens;
+        }
+
+        // Parser - Pattern matcher tokens fra brugeren og kalder funktionerne
+        static public void Parser(string[] tokens) {
+            for (int i = 0; i < tokens.Length; i++) {
+                if (tokens[i].Equals("insert")) {
+                    try {
+                        int val = Int32.Parse(tokens[i + 1]);
+                        tree.Insert(val);
+
+                    } catch (Exception) {
+                        Console.WriteLine("ERROR: 'insert' keyword must be followed by an integer");
+                        //throw;
+                    }
+                } else if (tokens[i].Equals("min")){
+                    try {
+                        Console.WriteLine(tree.min());
+                    } catch (Exception) {
+                        Console.WriteLine("ERROR: No values inserted");
+                        //throw;
+                    }
+                } else if (tokens[i].Equals("max")) {
+                    try {
+                        Console.WriteLine(tree.max());
+                    } catch (Exception) {
+                        Console.WriteLine("ERROR: No values inserted");
+                        //throw;
+                    }
+
+                } else if (tokens[i].Equals("find")) {
+                    try {
+                        int val = Int32.Parse(tokens[i + 1]);
+                        Console.WriteLine(tree.find(val));
+                    } catch (Exception) {
+                        Console.WriteLine("ERROR: 'find' keyword must be followed by an integer");
+                        //throw;
+                    }
+                } else if (tokens[i].Equals("exit")) {
+                    loop = false;
+                }
+            }
+        }
+
+
+    }
+}
